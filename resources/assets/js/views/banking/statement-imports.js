75bc6366f6de330b3a75d781accdaea3b77961a1
Wojtek Siudzinski <admin@suda.pl>
2026-03-13 20:23:00 +0100

    Add BulkAction plugin support to statement-imports view

--- resources/assets/js/views/banking/statement-imports.js
+++ resources/assets/js/views/banking/statement-imports.js
@@ -13,6 +13,7 @@ import DashboardPlugin from './../../plugins/dashboard-plugin';
 import Global from './../../mixins/global';
 
 import Form from './../../plugins/form';
+import BulkAction from './../../plugins/bulk-action';
 
 // plugin setup
 Vue.use(DashboardPlugin);
@@ -27,6 +28,7 @@ const app = new Vue({
     data: function () {
         return {
             form: new Form('statement-import'),
+            bulk_action: new BulkAction('statement-imports'),
         }
     },
 });
