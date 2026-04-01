ec6caefee6d0f190271a2361581a514477595027
Wojtek Siudzinski <admin@suda.pl>
2026-04-01 17:17:19 +0200

    Release v3.3.3

--- config/version.php
+++ config/version.php
@@ -10,15 +10,15 @@ return [
 
     'minor'     =>  '3',
 
-    'patch'     =>  '2',
+    'patch'     =>  '3',
 
     'build'     =>  '',
 
     'status'    =>  'Stable',
 
-    'date'      =>  '15-March-2026',
+    'date'      =>  '01-April-2026',
 
-    'time'      =>  '20:36',
+    'time'      =>  '15:17',
 
     'zone'      =>  'GMT +0',
 
